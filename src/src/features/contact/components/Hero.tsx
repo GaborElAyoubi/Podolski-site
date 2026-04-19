4040564d2a3ce35cce2888f3660d6838cccc46a4
Gabor <gabit8@protonmail.com>
2026-04-19 12:04:09 +0200

    before restructoring

---
+++ src/src/features/contact/components/Hero.tsx
@@ -0,0 +1,12 @@
+export function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>BERÜHRUNG</h1>
+      </div>
+
+      <img src="/feather.png" className="feather" />
+      <svg className="heart-line">...</svg>
+    </section>
+  );
+}
